f714642e8212b16e48d21bc9d3f8eefeab016d1a
nihilityer <hch2796945700@outlook.com>
2023-11-28 20:42:08 +0800

    fix: 优化mock组件依赖

--- src/communicat/mock.rs
+++ src/communicat/mock.rs
@@ -1,9 +1,10 @@
+use anyhow::Result;
 use async_trait::async_trait;
 use nihility_common::instruct::InstructReq;
+use nihility_common::manipulate::ManipulateReq;
 use nihility_common::response_code::RespCode;
 use tracing::warn;
-use anyhow::Result;
-use nihility_common::manipulate::ManipulateReq;
+
 use crate::communicat::{SendInstructOperate, SendManipulateOperate};
 
 #[derive(Default)]
@@ -16,7 +17,7 @@ pub struct MockManipulateClient;
 impl SendInstructOperate for MockInstructClient {
     async fn send(&mut self, instruct: InstructReq) -> Result<RespCode> {
         warn!("Mock Instruct Client Get Instruct: {:?}", instruct);
-        return Ok(RespCode::UnableToProcess)
+        return Ok(RespCode::UnableToProcess);
     }
 }
 
@@ -24,6 +25,6 @@ impl SendInstructOperate for MockInstructClient {
 impl SendManipulateOperate for MockManipulateClient {
     async fn send(&mut self, manipulate: ManipulateReq) -> Result<RespCode> {
         warn!("Mock Manipulate Client Get Manipulate: {:?}", manipulate);
-        return Ok(RespCode::UnableToProcess)
+        return Ok(RespCode::UnableToProcess);
     }
 }
