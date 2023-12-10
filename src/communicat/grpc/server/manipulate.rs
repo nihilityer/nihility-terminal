e20432a02b804c660c7f08f16509e0448e547ff5
nihilityer <hch2796945700@outlook.com>
2023-12-10 15:56:15 +0800

    feat: 实现Grpc组件流式处理Manipulate接口

--- src/communicat/grpc/server/manipulate.rs
+++ src/communicat/grpc/server/manipulate.rs
@@ -1,7 +1,11 @@
 use nihility_common::manipulate::manipulate_server::Manipulate;
 use nihility_common::manipulate::{SimpleManipulate, TextDisplayManipulate};
 use nihility_common::response_code::{Resp, RespCode};
+use tokio::spawn;
+use tokio::sync::mpsc;
 use tokio::sync::mpsc::UnboundedSender;
+use tokio_stream::wrappers::ReceiverStream;
+use tokio_stream::StreamExt;
 use tonic::{Request, Response, Status, Streaming};
 use tracing::error;
 
@@ -20,8 +24,9 @@ impl Manipulate for ManipulateImpl {
     ) -> Result<Response<Resp>, Status> {
         match self
             .manipulate_sender
-            .send(ManipulateEntity::create_by_simple_type_req(request.into_inner()))
-        {
+            .send(ManipulateEntity::create_by_simple_type_req(
+                request.into_inner(),
+            )) {
             Ok(_) => Ok(Response::new(Resp {
                 code: RespCode::Success.into(),
             })),
@@ -41,8 +46,9 @@ impl Manipulate for ManipulateImpl {
     ) -> Result<Response<Resp>, Status> {
         match self
             .manipulate_sender
-            .send(ManipulateEntity::create_by_text_type_req(request.into_inner()))
-        {
+            .send(ManipulateEntity::create_by_text_type_req(
+                request.into_inner(),
+            )) {
             Ok(_) => Ok(Response::new(Resp {
                 code: RespCode::Success.into(),
             })),
@@ -62,7 +68,66 @@ impl Manipulate for ManipulateImpl {
         &self,
         request: Request<Streaming<TextDisplayManipulate>>,
     ) -> Result<Response<Self::SendMultipleTextDisplayManipulateStream>, Status> {
-        todo!()
+        let mut req_stream = request.into_inner();
+        let (tx, rx) = mpsc::channel(128);
+        let manipualte_sender = self.manipulate_sender.clone();
+        spawn(async move {
+            while let Some(result) = req_stream.next().await {
+                match result {
+                    Ok(manipulate) => {
+                        match manipualte_sender
+                            .send(ManipulateEntity::create_by_text_type_req(manipulate))
+                        {
+                            Ok(_) => {
+                                match tx
+                                    .send(Ok(Resp {
+                                        code: RespCode::Success.into(),
+                                    }))
+                                    .await
+                                {
+                                    Ok(_) => {}
+                                    Err(e) => {
+                                        error!("Manipulate Server send_multiple_text_display_manipulate Send To Stream Error: {:?}", e);
+                                        break;
+                                    }
+                                }
+                            }
+                            Err(e) => {
+                                error!("Manipulate Server send_multiple_text_display_manipulate Send To Core Error: {:?}", e);
+                                match tx
+                                    .send(Ok(Resp {
+                                        code: RespCode::UnknownError.into(),
+                                    }))
+                                    .await
+                                {
+                                    Ok(_) => {}
+                                    Err(e) => {
+                                        error!("Manipulate Server send_multiple_text_display_manipulate Send To Stream Error: {:?}", e);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        error!(
+                            "Manipulate Server send_multiple_text_display_manipulate Receive Error: {:?}",
+                            &e
+                        );
+                        match tx.send(Err(e)).await {
+                            Ok(_) => {}
+                            Err(e) => {
+                                error!("Manipulate Server send_multiple_text_display_manipulate Send To Stream Error: {:?}", e);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        Ok(Response::new(
+            Box::pin(ReceiverStream::new(rx)) as Self::SendMultipleTextDisplayManipulateStream
+        ))
     }
 }
 
